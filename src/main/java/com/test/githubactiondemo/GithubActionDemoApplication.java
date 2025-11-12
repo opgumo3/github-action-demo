67f5147441638a7030aa3f8f40f0625d946f3179
opgumo <opgumo@naver.com>
2025-11-12 16:50:38 +0900

    init

---
+++ src/main/java/com/test/githubactiondemo/GithubActionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.test.githubactiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionDemoApplication.class, args);
+	}
+
+}
